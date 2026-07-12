2eddcc34a023a3dfbdde49c2d1282e3ce926fba6
ruishigure <chocolate.aruishigure@gmail.com>
2026-07-13 01:08:46 +0900

    commit 1

---
+++ gui/mainwindow.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+from PySide6.QtWidgets import (
+    QFileDialog,
+    QLabel,
+    QMainWindow,
+    QMessageBox,
+)
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.current_file = None
+
+        self.setWindowTitle("VoiceProjectEditor")
+        self.resize(1200, 700)
+
+        self.create_menu()
+
+        label = QLabel(
+            "VoiceProjectEditor\n\n"
+            "VOICEPEAK (.vpp)\n"
+            "VoiSona (.ccst)\n\n"
+            "Commit 1"
+        )
+
+        label.setStyleSheet("font-size:18px;")
+        label.setAlignment(
+            label.alignment()
+        )
+
+        self.setCentralWidget(label)
+
+    def create_menu(self):
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("ファイル")
+
+        open_action = file_menu.addAction("開く")
+
+        open_action.triggered.connect(
+            self.open_project
+        )
+
+        file_menu.addSeparator()
+
+        exit_action = file_menu.addAction("終了")
+        exit_action.triggered.connect(self.close)
+
+    def open_project(self):
+
+        filename, _ = QFileDialog.getOpenFileName(
+            self,
+            "プロジェクトを開く",
+            "",
+            "Voice Project (*.vpp *.ccst)"
+        )
+
+        if not filename:
+            return
+
+        self.current_file = Path(filename)
+
+        QMessageBox.information(
+            self,
+            "読み込み",
+            str(self.current_file)
+        )
